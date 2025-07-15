3ea7cf908fa743bac1b5cf88b2fe87c100167bf0
wayueee <farhanhokila30@gmail.com>
2025-07-15 19:03:13 +0700

    Updates

---
+++ assets/2gK-0NCW.js
@@ -0,0 +1,1 @@
+import{Q as s,r as o}from"./B5-UWauq.js";const i=s("shop",()=>{const t=o("easy"),e=o(1e4);return{difficulty:o({MIN_RANDOM_TIME:1e3,MAX_RANDOM_TIME:5e3}),mode:t,time:e}});export{i as u};
